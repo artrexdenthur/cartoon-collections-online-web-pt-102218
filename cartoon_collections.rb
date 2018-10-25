75c23d9dd58ef207ac46600e36e2fab3c2e3497b
Paul Ashour <paul.ashour@gmail.com>
2018-10-25 05:15:26 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarrows)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map { |call| puts "#{call."
+  planeteer_calls.map { |call| puts "#{call.capitalize}!" }
 end
 
 def long_planeteer_calls# code an argument here
