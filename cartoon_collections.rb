e06d5ab30b6ee8d48eba5b3dbe9f88e0f9057892
Paul Ashour <paul.ashour@gmail.com>
2018-10-25 05:16:50 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,11 +6,11 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |call| puts "#{call.capitalize}!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words_array)
+  words_array.any? { |word| word.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese_obj)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
