8bbcc1d085a64a8b062009a6542c22ce64a32673
Paul Ashour <paul.ashour@gmail.com>
2018-10-25 05:13:42 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,5 @@
 def roll_call_dwarves(dwarrows)
-  dwarrows.each_with_index { |dwarf, i| puts "#{i} #{dwarf}"
-  # Your code here
+  dwarrows.each_with_index { |dwarf, i| puts "#{i} #{dwarf}" }
 end
 
 def summon_captain_planet# code an argument here
